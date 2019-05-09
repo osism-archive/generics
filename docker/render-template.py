4a2d56894c1e7af082e36ce55c13d7122eab9469
Christian Berendt <berendt@betacloud-solutions.de>
2019-05-09 15:38:32 +0200

    docker: add support for versions

--- docker/render-template.py
+++ docker/render-template.py
@@ -13,7 +13,8 @@ environment = jinja2.Environment(loader=loader, keep_trailing_newline=True)
 template = environment.get_template(sys.argv[1])
 result = template.render({
     "docker_image_name": information.get("docker_image_name", "NONE"),
-    "readme_note": information.get("readme_note", None)
+    "readme_note": information.get("readme_note", None),
+    "versions": information.get("versions", ["latest"])
 })
 with open(sys.argv[1], "w+") as fp:
     fp.write(result)
