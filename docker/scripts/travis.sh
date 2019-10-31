1d8e2b04307daeb7cfcd1ca2075812bd81d45584
Christian Berendt <berendt@betacloud-solutions.de>
2019-10-31 17:56:49 +0100

    docker: use apt-get

--- docker/scripts/travis.sh
+++ docker/scripts/travis.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt update
-sudo apt -y install docker-ce
+sudo apt-get update
+sudo apt-get -y install docker-ce
 echo '{ "experimental": true }' | sudo tee /etc/docker/daemon.json
 sudo service docker restart
 
